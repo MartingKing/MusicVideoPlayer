7b38c340920c9f774b298beff04248491d64797c
denghuadong <dhd040805@gmail.com>
2018-03-28 18:08:44 +0800

    repair bug

--- app/src/main/java/com/huadong/musicvideoplayer/activity/PlayActivity.java
+++ app/src/main/java/com/huadong/musicvideoplayer/activity/PlayActivity.java
@@ -3,15 +3,12 @@ package com.huadong.musicvideoplayer.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.huadong.musicvideoplayer.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.vov.vitamio.MediaPlayer;
-import io.vov.vitamio.widget.MediaController;
-import io.vov.vitamio.widget.VideoView;
 
 /**
  * Created by xl on 2017/8/3.
@@ -19,8 +16,8 @@ import io.vov.vitamio.widget.VideoView;
 
 public class PlayActivity extends AppCompatActivity {
 
-    @BindView(R.id.videoView)
-    VideoView videoView;
+    @BindView(R.id.video_view)
+    VideoView mVideoView;
     private String videoPath;
     private String videtitle;
 
@@ -30,30 +27,14 @@ public class PlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play);
         ButterKnife.bind(this);
         initview();
-        playfunction();
     }
 
     private void initview() {
         Bundle bundle = getIntent().getExtras();
         videoPath = (String) bundle.getSerializable("videopath");
         videtitle = (String) bundle.getSerializable("videtitle");
+        mVideoView.setVideoPath(videoPath);
+        mVideoView.start();
     }
 
-    void playfunction() {
-        if (videoPath.equals("")) {
-            Toast.makeText(PlayActivity.this, "视频路径错误", Toast.LENGTH_SHORT).show();
-        } else {
-            videoView.setVideoPath(videoPath);
-            videoView.setMediaController(new MediaController(this));
-            videoView.requestFocus();
-
-            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mediaPlayer) {
-                    // optional need Vitamio 4.0
-                    mediaPlayer.setPlaybackSpeed(1.0f);
-                }
-            });
-        }
-    }
 }
